65cc219949e7762e4915e289515b2adeaf6f0439
nvm <Never M>
2019-12-16 18:16:26 +0200

    Render posts in controller

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Post;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,8 +13,10 @@ class PostController extends AbstractController
      */
     public function index()
     {
+        $doctrine = $this->getDoctrine();
+        $repo = $doctrine->getRepository(Post::class);
         return $this->render('post/index.html.twig', [
-            'controller_name' => 'PostController',
+            'posts' => $repo->findAll(),
         ]);
     }
 }
