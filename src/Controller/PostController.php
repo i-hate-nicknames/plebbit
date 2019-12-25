c6881c7b047c359f09f52f023091bc0f9b8d475d
nvm <nevermn@protonmail.com>
2019-12-25 15:57:30 +0200

    Add post form

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -3,20 +3,36 @@
 namespace App\Controller;
 
 use App\Entity\Post;
+use App\Forms\PostType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PostController extends AbstractController
 {
     /**
-     * @Route("/post", name="post")
+     * @Route("/posts", name="posts")
      */
-    public function index()
+    public function index(Request $request)
     {
+        $newPost = new Post();
+        $form = $this->createForm(PostType::class, $newPost);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $product = $form->getData();
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($product);
+            $entityManager->flush();
+            return $this->redirectToRoute('posts');
+        }
+
         $doctrine = $this->getDoctrine();
         $repo = $doctrine->getRepository(Post::class);
         return $this->render('post/index.html.twig', [
             'posts' => $repo->findAll(),
+            'form' => $form->createView()
         ]);
     }
 }
